2dc2660b6ca1fa73aa7b28242cc04f8a2a1fb0a2
Xcorpiion <vinicius.da.silva.cruz,sanji@gmail.com>
2020-04-15 00:10:02 -0300

    Movimentos iniciais do personagem
    Implementação da animação
    configuração das animação usando o animation e o animator
    criação da model e fazendo os testes

--- Assets/Script/Teste.cs
+++ Assets/Script/Teste.cs
@@ -4,12 +4,46 @@ using UnityEngine;
 
 public class Teste : MonoBehaviour {
     // Start is called before the first frame update
-    void Start() {
 
+    public Player player;
+
+    private int idAnimation;
+
+    private bool grounded = true;
+
+    public float horizontal;
+
+    void Start() {
+        // Method intentionally left empty.
+        player = this.GetComponent<Player>();
+        player.setAnimator(this.GetComponent<Animator>());
+        
     }
 
     // Update is called once per frame
     void Update() {
-        Animator playerAnimator;
+        // Method intentionally left empty.
+        movimentacao();
+        fazAnimacoes();
+
+    }
+
+    private void fazAnimacoes() {
+        player.getAnimator().SetInteger("idAnimation", idAnimation);
+        player.getAnimator().SetBool("grounded", grounded);
+    }
+
+    private void movimentacao() {
+        horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+        if (horizontal != 0) {
+            this.idAnimation = 1;
+        } else {
+            this.idAnimation = 0;
+        }
+
+        if (vertical < 0) {
+            this.idAnimation = 2;
+        }
     }
 }
---
+++ Assets/Script/model/Player.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    public Animator animator;
+
+    public void setAnimator(Animator animator) {
+        this.animator = animator;
+    }
+
+    public Animator getAnimator() {
+        return this.animator;
+    }
+
+}
