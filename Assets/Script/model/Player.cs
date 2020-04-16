8c84d41e6a4d5424edbba331d675800053a3eddf
Xcorpiion <vinicius.da.silva.cruz,sanji@gmail.com>
2020-04-16 00:35:15 -0300

    Adicionando movimentação do player e arrumando a sua colisão

--- Assets/Script/model/Player.cs
+++ Assets/Script/model/Player.cs
@@ -3,7 +3,17 @@ using UnityEngine;
 
 public class Player : MonoBehaviour {
 
-    public Animator animator;
+    private Animator animator;
+
+    private Rigidbody2D rigidbody;
+
+    public void setRigidbody(Rigidbody2D rigidbody) {
+        this.rigidbody = rigidbody;
+    }
+
+    public Rigidbody2D getRigidbody() {
+        return this.rigidbody;
+    }
 
     public void setAnimator(Animator animator) {
         this.animator = animator;
